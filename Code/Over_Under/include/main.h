8854f3a50dd6ce9a1b22d4de3d439c6d10a70140
Daniel <danielkohler737@gmail.com>
2023-10-29 12:52:24 -0500

    ARMS Odometry
    Changed to ARMS for drivetrain control. Autonomous routines not complete.

--- Code/Over_Under/include/main.h
+++ Code/Over_Under/include/main.h
@@ -15,6 +15,9 @@
 #ifndef _PROS_MAIN_H_
 #define _PROS_MAIN_H_
 
+#include "ARMS/api.h"
+
+
 /**
  * If defined, some commonly used enums will have preprocessor macros which give
  * a shorter, more convenient naming pattern. If this isn't desired, simply
--- Code/Over_Under/src/main.cpp
+++ Code/Over_Under/src/main.cpp
@@ -1,13 +1,17 @@
 #include "main.h"
-#include "okapi/api/chassis/controller/chassisController.hpp"
-#include "okapi/api/control/async/asyncMotionProfileController.hpp"
+#include "ARMS/chassis.h"
+#include "ARMS/flags.h"
 #include "okapi/api/device/motor/abstractMotor.hpp"
-//#include "gif-pros/gifclass.hpp"
+#include "gif-pros/gifclass.hpp"
 #include "okapi/impl/control/async/asyncMotionProfileControllerBuilder.hpp"
 #include "okapi/impl/device/button/controllerButton.hpp"
 #include "okapi/impl/device/controllerUtil.hpp"
 #include "okapi/impl/device/distanceSensor.hpp"
 #include "pros/adi.hpp"
+#include "pros/misc.h"
+#include "pros/rtos.hpp"
+#include <cstdlib>
+#include "ARMS/config.h"
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -15,7 +19,11 @@
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {}
+
+void initialize() {
+  arms::init();
+  pros::lcd::initialize();
+}
 
 /**
  * Runs while the robot is in the disabled state of Field Management System or
@@ -33,13 +41,17 @@ void disabled() {}
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void competition_initialize() {}
+void competition_initialize() {
+//Gif gif("/usd/bk.gif", lv_scr_act());
+}
 
 Motor cataMotor(8);
 Motor intakeMotor(-7);
 ADIButton cataLimit('B');
 DistanceSensor intakeSensor(9);
 pros::ADIDigitalOut flap('A');
+pros::ADIDigitalOut hangLeft('D');
+pros::ADIDigitalOut hangRight('E');
 
 
 Controller controller;        //Defines the controller
@@ -49,121 +61,79 @@ ControllerButton cataShootManual(ControllerDigital::R2);
 //
 ControllerButton intakeIN(ControllerDigital::L1);
 ControllerButton intakeOUT(ControllerDigital::L2);
-
-//ControllerButton flapOUT(ControllerDigital::up);
-//ControllerButton flapIN(ControllerDigital::down);
+//
 ControllerButton flapToggle(ControllerDigital::down);
 
-
-std::shared_ptr<ChassisController> chassis =
-ChassisControllerBuilder()
-    .withMotors({-1, -2, -3}, {4, 5, 6})
-    // Green gearset, 3.25 in wheel diam, 9.4 in wheel track
-    .withDimensions({AbstractMotor::gearset::blue , (60.0 / 36.0)}, {{3.25_in, 11_in}, imev5BlueTPR})
-    .withMaxVelocity(600)
-    .build();
-
-std::shared_ptr<AsyncMotionProfileController> profileController =
-AsyncMotionProfileControllerBuilder()
-  .withLimits({
-    1, //max linear velocity
-    2.0, //max linear acceleration
-    10.0 //max jerk
-  })
-.withOutput(chassis)
-.buildMotionProfileController();
+ControllerButton hang(ControllerDigital::X);
+pros::Controller master (pros::E_CONTROLLER_MASTER);
 
 
-void autonomousroutine1() {
-  chassis->setMaxVelocity(250);
+void CloseSideAWP() {
+arms::odom::reset({0, 0}, -135);
 
-chassis->moveDistance(18_in);
+arms::chassis::move({21,16, 180}, 80, arms::REVERSE);
+arms::chassis::move({28,16, 180}, 80, arms::REVERSE);
 
-chassis->turnAngle(45_deg);
-chassis->moveDistance(7_in);
-intakeMotor.moveVoltage(-12000);
+
+arms::chassis::move({-1,4}, 80, arms::ASYNC);
 pros::delay(500);
-
-intakeMotor.moveVoltage(0);
-chassis->moveDistance(-7_in);
-
-chassis->turnAngle(180_deg);
-chassis->moveDistance(-13_in);
-chassis->moveDistance(12_in);
-
-chassis->turnAngle(-40_deg);
-
-
-chassis->moveDistanceAsync(19_in);
 flap.set_value(true);
-chassis->waitUntilSettled();
-
-chassis->turnAngleAsync(-50_deg);
+arms::chassis::waitUntilFinished(2);
+arms::chassis::turn(-90);
 flap.set_value(false);
-chassis->waitUntilSettled();
 
-chassis->setMaxVelocity(350);
-
-intakeMotor.moveVoltage(-12000);
-chassis->moveDistance(37_in);
-
-pros::delay(2000);
-intakeMotor.moveVoltage(0);
+arms::chassis::move({-1,-39, -90}, 80);
 
 }
 
-void autonomousroutine2() { //flap slap preload, go to pick up one and grab other with flap, go for third
-//cata thing
 
-profileController->generatePath({
-    {0_ft, 0_ft, 0_deg},
-    {32_ft, 0_ft, 0_deg}},
-    "A"
-    );
-    profileController->setTarget("A");
-    profileController->waitUntilSettled();
+void FarSideAWP() {
+arms::odom::reset({0, -24}, 90);
 
-chassis->moveDistance(-12_in);
+arms::chassis::move({0,-26}, 60);
 
-chassis->turnAngle(75_deg);
-intakeMotor.moveVoltage(-12000);
-pros::delay(1000);
+while (intakeSensor.get() > 50) {
+	intakeMotor.moveVoltage(12000);
+}
 intakeMotor.moveVoltage(0);
-chassis->turnAngle(-165_deg);
-chassis->moveDistance(15_in);
 
-flap.set_value(true);
-chassis->moveDistance(-32_in);
+arms::chassis::move({21,-16, 180}, 80, arms::REVERSE);
+arms::chassis::move({28,-16, 180}, 80, arms::REVERSE);
 
-chassis->moveDistance(5_in);
-flap.set_value(false);
-
-chassis->turnAngle(15_deg);
-chassis->moveDistance(36_in);
-
-//intake motor thing
-
-chassis->moveDistance(-36_in);
-chassis->turnAngle(75_deg);
-chassis->moveDistanceAsync(5_in);
+arms::chassis::move({23,-16}, 80);
+arms::chassis::turn(0);
+arms::chassis::move({30,-16}, 80, arms::ASYNC);
 intakeMotor.moveVoltage(-12000);
+arms::chassis::waitUntilFinished(2);
+pros::delay(550);
 
-chassis->waitUntilSettled();
-chassis->moveDistance(-4_in);
+arms::chassis::move({23,-16}, 80, arms::REVERSE);
+
+
+
 
 }
 
+
+void CloseSideBlitz() {
+
+}
+
+
+void FarSideBlitz() {
+
+}
+
+
+void closeSidePush() {
+
+
+}
+
+void farSideFullScore() {
+}
 
 void skillsroutine() {
-flap.set_value(true);
-
-chassis->moveDistanceAsync(-10_in);
-pros::delay(250);
-flap.set_value(false);
-chassis->waitUntilSettled();
-
-chassis->turnAngle(90_deg);
-chassis->moveDistance(4_in);
 
 int cataCounter = 0;
 while (cataCounter < 46) {
@@ -173,49 +143,49 @@ while (cataCounter < 46) {
   }
   pros::delay(10);
 }
-  //cata thing
-
-chassis->turnAngle(-75_deg);
-
-
-
-flap.set_value(true);
-chassis->moveDistance(-24_in);
-flap.set_value(false);
-chassis->turnAngle(-10_deg);
-chassis->moveDistance(10_in);
-chassis->turnAngle(10_deg);
-chassis->moveDistance(-9_in);
-
-
-};
-
-void autonTesting() {
-  profileController->generatePath({
-    {0_ft, 0_ft, 0_deg},
-    {2_ft, 1_ft, 90_deg}},
-    "A"
-    );
-
-  //  profileController->setTarget("A");
-    //profileController->waitUntilSettled();
-
-    //pros::delay(3000);
-
-  profileController->generatePath({
-    {0_ft, 0_ft, 0_deg},
-    {2_ft, -1_ft, -90_deg},
-    {2_ft, -2_ft, -90_deg}},
-    "B"
-    );
-
-    profileController->setTarget("B");
-    profileController->waitUntilSettled();
 }
 
-void autonomous() {
-  autonomousroutine1();
-//autonTesting();
+
+void autonomous() { ////8deg off
+CloseSideAWP();
+ //   arms::odom::reset({0, 0}, 0);
+ // arms::chassis::move(24, arms::ASYNC);
+
+  //    arms::odom::reset({0, 0}, 0);
+//arms::chassis::turn(90);
+//arms::chassis::move({12, 0}, 50);
+
+//pros::delay(500);
+//arms::chassis::move({0, 0}, 50);
+
+//arms::chassis::turn(0);
+//pros::delay(500);
+//arms::chassis::turn(90);
+
+
+//arms::chassis::move({6, 0}, 50);
+
+}
+
+void Aautonomous() {
+if(arms::selector::auton == 1) {
+  CloseSideAWP();
+}
+if(arms::selector::auton == 2) {
+  CloseSideBlitz();
+}
+if(arms::selector::auton == 3) {
+}
+
+if(arms::selector::auton == -1) {
+  FarSideAWP();
+}
+if(arms::selector::auton == -2) {
+  FarSideBlitz();
+}
+if(arms::selector::auton == -3) {
+}
+
 } 
 
 
@@ -226,9 +196,9 @@ if (cataShoot.isPressed()){                           //If catapult shoot button
     pros::delay(350);               //Delay to ensure catapult shoots
     }
   while (!cataLimit.isPressed()) {                //While the catapult limit switch is not pressed
-    cataMotor.moveVoltage(8000);    //Move the catapult down
-     chassis->getModel()->tank(controller.getAnalog(ControllerAnalog::leftY),
-                                 controller.getAnalog(ControllerAnalog::rightY));
+    cataMotor.moveVoltage(10000);    //Move the catapult down
+      arms::chassis::tank(master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y),
+                        master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
   pros::delay(10);      // ^ Allows for driving while the catapult is going down
   }
 cataMotor.moveVoltage(0);   //Stop the catapult once the limit switch is pressed
@@ -236,38 +206,54 @@ cataMotor.moveVoltage(0);   //Stop the catapult once the limit switch is pressed
 }
 
 void setCata() {
-    if (cataShootManual.isPressed()) {
+    if (cataShootManual.isPressed()) {            
         cataMotor.moveVoltage(12000);
+    }
+    else {
+      cataMotor.moveVoltage(0);
     }
 }
 
 
 void setIntake() {
 	if (intakeIN.isPressed()) {
-		intakeMotor.moveVoltage(12000);
+		intakeMotor.moveVoltage(12000);   //If intake button is pressed then intake
 	}
-	else if (intakeOUT.isPressed()) {
+	else if (intakeOUT.isPressed()) {             //If outtake button is pressed then outtake
 		intakeMotor.moveVoltage(-12000);
 	}
 	else {
-		if (intakeSensor.get() < 50) {
+		if (intakeSensor.get() < 50) {              //If the sensor sees a triball in the intake, stop intake
 			intakeMotor.moveVoltage(0);
 		}
   else {
-	  intakeMotor.moveVoltage(12000);
+	  intakeMotor.moveVoltage(12000);   //By default keep intake on
 	  }
 	}
 }
 
 
 void setFlap() {
-  if (flapToggle.isPressed()) {
-    flap.set_value(true);
+  if (flapToggle.isPressed()) {   //If flap button is pressed
+    flap.set_value(true);   //Extend flap
   }
   else {
-    flap.set_value(false);
+    flap.set_value(false);      //Otherwise keep flap closed
   }
 }
+
+
+void setHang() {
+  if (hang.isPressed()) {
+    hangLeft.set_value(true);
+    hangRight.set_value(true);
+  }
+  else {
+    hangLeft.set_value(false);
+    hangRight.set_value(false);
+  }
+}
+
 /**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -285,12 +271,32 @@ void setFlap() {
 
 void opcontrol() {
 	while (true) {
-    chassis->getModel()->tank(controller.getAnalog(ControllerAnalog::leftY),
-                            controller.getAnalog(ControllerAnalog::rightY));
-  resetCata();
-  setCata();
-	setIntake();
-  setFlap();
-	pros::delay(10);
+    arms::chassis::tank(master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y),
+                        master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
+    resetCata();
+    setCata();
+    setIntake();
+    setFlap();
+    setHang();
+    pros::lcd::set_text(0, "X: " + std::to_string(arms::odom::getPosition().x));
+    pros::lcd::set_text(1, "Y: " + std::to_string(arms::odom::getPosition().y));
+    pros::lcd::set_text(2, "H: " + std::to_string(arms::odom::getHeading()));
+    pros::lcd::set_text(3, "Left: " + std::to_string(arms::odom::getLeftEncoder()));
+    pros::lcd::set_text(4, "Right: " + std::to_string(arms::odom::getRightEncoder()));
+    pros::lcd::set_text(5, "Middle: " + std::to_string(arms::odom::getMiddleEncoder()));
+
+    pros::delay(10);
 }
+}
+void oOpcontrol() {
+  while (true) {
+    // insert other opcontrol code here
+    pros::lcd::set_text(0, "X: " + std::to_string(arms::odom::getPosition().x));
+    pros::lcd::set_text(1, "Y: " + std::to_string(arms::odom::getPosition().y));
+    pros::lcd::set_text(2, "H: " + std::to_string(arms::odom::getHeading()));
+    pros::lcd::set_text(3, "Left: " + std::to_string(arms::odom::getLeftEncoder()));
+    pros::lcd::set_text(4, "Right: " + std::to_string(arms::odom::getRightEncoder()));
+    pros::lcd::set_text(5, "Middle: " + std::to_string(arms::odom::getMiddleEncoder()));
+    pros::delay(10);
+  }
 }
