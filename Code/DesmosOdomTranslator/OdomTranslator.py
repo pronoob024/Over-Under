0c12e1e81c5046fa37def94ee4008f5a29e0f052
Daniel <danielkohler737@gmail.com>
2024-02-12 22:00:42 -0600

    Commented OdomTranslator Script
    Added comments to script to explain what code is doing

--- Code/DesmosOdomTranslator/OdomTranslator.py
+++ Code/DesmosOdomTranslator/OdomTranslator.py
@@ -1,17 +1,17 @@
-initypos, initxpos, resetangle = input("Starting Points and Angle: ").split()
+initypos, initxpos, resetangle = input("Starting Points and Angle: ").split()  # Take an input for the initial points
 
-print("arms::odom::reset({0, 0}, " + resetangle + ");")  # initial position reset
+print("arms::odom::reset({0, 0}, " + resetangle + ");")  # Output initial position reset
 
-initypos = float(initypos)
-initxpos = float(initxpos)
+initypos = float(initypos)  # Converts input into a float
+initxpos = float(initxpos)  # Converts input into a float
 
 while True:
-    newx, newy = input("Input Points: ").split()
-    newxflt = float(newy)
-    newyflt = float(newx)
-    newx = newxflt - initxpos
-    newy = newyflt - initypos
-    newy *= -1
-    newx = str(newx)
-    newy = str(newy)
-    print("arms::chassis::move({" + newx + ", " + newy + "}, 100);")
+    newx, newy = input("Input Points: ").split()  # Takes input for point
+    newxflt = float(newy)  # Converts Y direction into X direction
+    newyflt = float(newx)  # Converts X direction into Y direction
+    newx = newxflt - initxpos  # Makes X position relative to 0, 0 starting position
+    newy = newyflt - initypos  # Makes Y position relative to 0, 0 starting position
+    newy *= -1  # Gives Y direction correct sign
+    newx = str(newx)  # Converts X position into string for outputting
+    newy = str(newy)  # Converts Y position into string for outputting
+    print("arms::chassis::move({" + newx + ", " + newy + "}, 100);")  # Output movement command
